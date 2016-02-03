7f4fe4b537463577a4b331116b105f616962a8d1
Michael Freeman <michael.k.freeman@gmail.com>
2016-02-03 09:48:51 -0800

    Cleaned exercises

--- exercise1.R
+++ exercise1.R
@@ -3,21 +3,19 @@ library(jsonlite)
 
 # Create a variable `api_key` that stores your api key
 # Register your own API key here: https://developer.echonest.com/account/register
-api_key <- 'Y0SBZBEY1KHGBJKVR'
+
 
 # Create a variable that stores the name of your favorite artist
-artist <- 'Trampled by turtles'
+
 
 # Substitute the spaces in your band-name with '+'s using the `gsub` function
-artist_no_spaces <- gsub(' ', '+', artist)
+
 
 # Construct a seach query to hit the api.
 # See documentation/examples: http://developer.echonest.com/docs/v4/artist.html#biographies
-base <- 'http://developer.echonest.com/api/v4/artist/biographies?api_key='
-search <- paste0(base, api_key, '&name=', artist_no_spaces, '&results=1')
+
 
 # Use fromJSON to retrieve the results
-results <- fromJSON(search)
+
 
 # Get the text of the (first) biography available
-bio <- results$response$biographies$text
--- exercise2.md
+++ exercise2.md
@@ -9,13 +9,3 @@ Edit this Markdown document below to display:
 - An image of the band (which you should download from the web, and display)
 
 ----
-## Trampled by Turtles
-Trampled by Turtles is an incredible bluegrass band from Duluth, MN.  Some of their best songs are:
-
-- Schoolbus Driver
-- November
-- The Darkness and the Light
-
-Here's a photo of them:
-
-![tbt](tbt.jpeg)
