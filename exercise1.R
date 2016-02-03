5099d02243a3cae762b82f4324d5677e1f895ab9
Michael Freeman <michael.k.freeman@gmail.com>
2016-02-03 09:43:38 -0800

    Minor edits to exercise 1

--- exercise1.R
+++ exercise1.R
@@ -6,12 +6,15 @@ library(jsonlite)
 api_key <- 'Y0SBZBEY1KHGBJKVR'
 
 # Create a variable that stores the name of your favorite artist
-artist <- 'Trampled+by+turtles'
+artist <- 'Trampled by turtles'
+
+# Substitute the spaces in your band-name with '+'s using the `gsub` function
+artist_no_spaces <- gsub(' ', '+', artist)
 
 # Construct a seach query to hit the api.
 # See documentation/examples: http://developer.echonest.com/docs/v4/artist.html#biographies
 base <- 'http://developer.echonest.com/api/v4/artist/biographies?api_key='
-search <- paste0(base, api_key, '&name=', artist, '&results=1')
+search <- paste0(base, api_key, '&name=', artist_no_spaces, '&results=1')
 
 # Use fromJSON to retrieve the results
 results <- fromJSON(search)
